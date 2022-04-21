7d67d2b89d75835e26a4422552d41cf6be5da067
Lucas Alface <alfacetj1@gmail.com>
2022-04-21 16:29:44 +0200

    Em andamento

---
+++ index.js
@@ -0,0 +1,27 @@
+(() =>{
+  const form = document.querySelector('.dados-do-livro')
+  const tituloLivro = document.querySelector('.titulo-do-livro')
+  const paginasLivro = document.querySelector('.pagina-do-livro')
+  const autorLivro = document.querySelector('.autor-do-livro')
+  const meuLivro =[];
+
+  form.addEventListener('submit', (e) =>{
+      e.preventDefault();
+
+      
+       
+function livro(){
+
+  }
+
+function adiconeLivro(){
+
+  }
+
+  })
+
+  
+
+  
+
+})();
