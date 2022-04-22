9f7e458a32144c6c4abdc9346831367b558041ac
Lucas Alface <alfacetj1@gmail.com>
2022-04-22 09:05:18 +0200

    Em andamento

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 (() =>{
   const form = document.querySelector('.dados-do-livro')
-  const title= document.querySelector('.titulo-do-livro')
-  const pages = document.querySelector('.pagina-do-livro')
-  const author = document.querySelector('.autor-do-livro');
+  const title= document.querySelector('.titulo')
+  const pages = document.querySelector('.pagina')
+  const author = document.querySelector('.autor');
   const reader = document.querySelector('.leitura')
-  const myBook =[];
+  const myLibrary =[];
 
   form.addEventListener('submit', (e) =>{
     e.preventDefault()
     
     if(title.value === '' && pages.value === '' &&  author.value === ''){
-      alert ('Preencha todos os campos obrigatórios!')
+      alert ('Preencha todos os campos obrigatórios!');
+      return
     }
+
+  
+     function Book(title, author, pages, reader){
+            this.title = title
+            this.autor = author
+            this.page = pages
+            this.leitotr = reader
+     }
+
+    const addBook = (title.value, author.value, pages.value, reader.value);
+      myLibrary.push(addBook)
+      console.log(myLibrary)
+    
     
   })
 
